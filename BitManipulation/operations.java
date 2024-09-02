f338e9fbc12e9ea90a6dd0076e4fc9691e750ea3
Sushant Nalage <145271874+sushant054@users.noreply.github.com>
2024-09-02 23:48:25 +0530

    opration:set, get , clear,update..

--- BitManipulation/operations.java
+++ BitManipulation/operations.java
@@ -1,6 +1,8 @@
+import java.util.*;
 public class operations {
     public static void main(String[] args) {
         int n=5;
+        //5 in binary 0101
         int pos=1;
         int bitmask= 1<<pos;
         //to get bit
@@ -10,12 +12,29 @@ public class operations {
             System.out.println("Bot was one");
         }
         //set bit 
-        int newNumber= n | bitmask;
-        System.out.println("NO after setting bit: "+newNumber);
+        // int newNumber= n | bitmask;
+        // System.out.println("NO after setting bit: "+newNumber);
         //here 5---->7 we changed 1st position..
         //clear ith bit
-        int bitMask=~(1<<pos);
+        // int bitMask=~(1<<pos);
+        // int newNo= bitMask & n;
+        // System.out.println("new no after clear pos.. bit: "+newNo);
+
+        //update
+        Scanner sc = new Scanner(System.in);
+        System.out.println("operation: enter 0 for set and enter 1 for clear");
+        int operation =sc.nextInt();
+        if(operation==1){
+            //clear..
+            int bitMask=~(1<<pos);
         int newNo= bitMask & n;
         System.out.println("new no after clear pos.. bit: "+newNo);
+        }else
+        {//set
+            int newNumber= n | bitmask;
+            System.out.println("NO after setting bit: "+newNumber);
+        }
+
+
     }
 }
